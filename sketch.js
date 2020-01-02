c12a5dfa9780fe56b8b51df513904a0eb080bfb4
Yuvraj-hub <59394427+Yuvraj-hub@users.noreply.github.com>
2020-01-02 19:49:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var fixrect,movingrect;
+
+
+function setup() {
+  createCanvas(800,400);
+  fixrect= createSprite(200, 200, 80, 50);
+  fixrect.shapeColor="green";
+  movingrect = createSprite(400,300,50,100);
+  movingrect.shapeColor="green";
+}
+
+function draw() {
+  background(255,255,255);  
+ movingrect.x=World.mouseX;
+ movingrect.y=World.mouseY;
+
+ if (fixrect.x-movingrect.x<movingrect.width/2+fixrect.width/2 &&
+  movingrect.x-fixrect.x<movingrect.width/2+fixrect.width/2 &&
+  fixrect.y-movingrect.y<movingrect.height/2+fixrect.height/2 &&
+  movingrect.y-fixrect.y<movingrect.height/2+fixrect.height/2){
+  movingrect.shapeColor="red";
+  fixrect.shapeColor="red";
+  }
+
+  else {
+  movingrect.shapeColor = "green";
+  fixrect.shapeColor = "green";
+  }
+
+
+
+  drawSprites();
+}
